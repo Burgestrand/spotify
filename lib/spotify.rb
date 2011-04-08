7013e4f9a9e22b1e40a3d96944364a5078e85fca
Kim Burgestrand <kim@burgestrand.se>
2011-04-08 17:48:05 +0200

    Add definitions from libspotify Error Handling section

--- lib/spotify.rb
+++ lib/spotify.rb
@@ -15,4 +15,22 @@ module Spotify
   # libspotify API version
   # @return [Fixnum]
   API_VERSION = VERSION.split('.').first.to_i
+  
+  #
+  # Error
+  # 
+
+  # @see http://developer.spotify.com/en/libspotify/docs/group__error.html
+  enum :error, [:ok, :bad_api_version, :api_initialization_failed,
+               :track_not_playable, :resource_not_loaded,
+               :bad_application_key, :bad_username_or_password,
+               :user_banned, :unable_to_contact_server,
+               :client_too_old, :other_permanent, :bad_user_agent,
+               :missing_callback, :invalid_indata,
+               :index_out_of_range, :user_needs_premium,
+               :other_transient, :is_loading, :no_stream_available,
+               :permission_denied, :inbox_is_full, :no_cache,
+               :no_such_user]
+  
+  attach_function :error_message, :sp_error_message, [ :error ], :string
 end
