829374131123d468e1204283f82c6cf43a03e939
Kim Burgestrand <kim@burgestrand.se>
2011-04-08 17:43:48 +0200

    Add YARDocstring to Spotify module

--- lib/spotify.rb
+++ lib/spotify.rb
@@ -1,8 +1,13 @@
 # coding: utf-8
 require 'ffi'
-
 require 'spotify/version'
 
+# FFI wrapper around libspotify.
+# 
+# See official documentation for more detailed documentation about
+# functions and their behavior.
+# 
+# @see http://developer.spotify.com/en/libspotify/docs/
 module Spotify
-  
+
 end
