35882bc7918604adf77c66cb797242b4b0bf601b
Kim Burgestrand <kim@burgestrand.se>
2011-07-10 14:23:59 +0200

    Add specs for return type control of functions

--- spec/spotify_spec.rb
+++ spec/spotify_spec.rb
@@ -6,24 +6,24 @@ end
 
 #
 # Hooking FFI for extra introspection
-# 
+#
 require 'ffi'
 
 module Spotify
   extend FFI::Library
   extend self
-  
+
   def attach_function(name, func, arguments, returns = nil, options = nil)
     args = [name, func, arguments, returns, options].compact
     args.unshift name.to_s if func.is_a?(Array)
-    
+
     hargs = [:name, :func, :args, :returns].zip args
     @attached_methods ||= {}
     @attached_methods[name.to_s] = hash = Hash[hargs]
-    
+
     super
   end
-  
+
   attr_reader :attached_methods
 end
 
@@ -31,20 +31,20 @@ Bundler.require(:default, :development)
 
 #
 # Utility
-# 
+#
 API_H_PATH = File.expand_path('../api.h', __FILE__)
 API_H_SRC  = File.read API_H_PATH
 API_H_XML  = RbGCCXML.parse(API_H_PATH)
 
 #
 # General
-# 
+#
 describe Spotify do
   describe "VERSION" do
     it "should be defined" do
       defined?(Spotify::VERSION).must_equal "constant"
     end
-    
+
     it "should be the same version as in api.h" do
       spotify_version = API_H_SRC.match(/#define\s+SPOTIFY_API_VERSION\s+(\d+)/)[1]
       Spotify::API_VERSION.must_equal spotify_version.to_i
@@ -56,19 +56,39 @@ describe "functions" do
   API_H_XML.functions.each do |func|
     next unless func["name"] =~ /\Asp_/
     attached_name = func["name"].sub(/\Asp_/, '')
-    
+
+    def type_of(return_type)
+      return case return_type.to_cpp
+        when "const char*" then :string
+        else :pointer
+      end if return_type.is_a?(RbGCCXML::PointerType)
+
+      case return_type["name"]
+      when "unsigned int"
+        :uint
+      else
+        return_type["name"].sub(/\Asp_/, '').to_sym
+      end
+    end
+
     describe func["name"] do
       it "should be attached" do
         Spotify.must_respond_to attached_name
       end
-      
+
       it "should expect the correct number of arguments" do
         Spotify.attached_methods[attached_name][:args].count.
           must_equal func.arguments.count
       end
-      
-      # it "should expect the correct types of arguments"
-      # it "should return the correct type"
+
+      it "should return the correct type" do
+        current = Spotify.attached_methods[attached_name][:returns]
+        actual  = type_of(func.return_type)
+
+        Spotify.find_type(current).must_equal Spotify.find_type(actual)
+      end
+
+      it "should expect the correct types of arguments"
     end
   end
 end
@@ -76,26 +96,26 @@ end
 describe "enums" do
   API_H_XML.enumerations.each do |enum|
     attached_enum = Spotify.enum_type enum["name"].sub(/\Asp_/, '').to_sym
-    original_enum = enum.values.map { |v| [v["name"].downcase, v["init"]] } # TODO: SP_BITRATE_X => X
-    
+    original_enum = enum.values.map { |v| [v["name"].downcase, v["init"]] }
+
     describe enum["name"] do
       it "should exist" do
         attached_enum.wont_be_nil
       end
-      
+
       it "should match the definition" do
         attached_enum_map = attached_enum.symbol_map
         original_enum.each do |(name, value)|
           a_name, a_value = attached_enum_map.find { |(n, v)| name.match n.to_s }
           attached_enum_map.delete(a_name)
-          
+
           unless a_value.to_s == value.to_s
               p enum["name"]
               p [name, value]
               p [a_name, a_value]
               puts
           end
-          
+
           a_value.to_s.must_equal value
         end
       end
@@ -106,13 +126,13 @@ end
 describe "structs" do
   API_H_XML.structs.each do |struct|
     next if struct["incomplete"]
-    
+
     attached_struct = Spotify.constants.find do |const|
       struct["name"].gsub('_', '').match(/#{const}/i)
     end
-    
+
     attached_members = Spotify.const_get(attached_struct).members.map(&:to_s)
-    
+
     describe struct["name"] do
       it "should contain the same attributes" do
         struct.variables.map(&:name).each do |member|
