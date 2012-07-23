52682b5c58d596d07e015ad4f64b905d32d9d0cc
Kim Burgestrand <kim@burgestrand.se>
2012-07-23 03:23:57 +0200

    Bump version to v12.3.0.0

--- lib/spotify/version.rb
+++ lib/spotify/version.rb
@@ -1,7 +1,7 @@
 module Spotify
   # @note See README for versioning policy.
   # @return [String] Spotify gem version.
-  VERSION = '12.2.0'
+  VERSION = '12.3.0.0'
 
   # @note May or may not work with other versions.
   # @return [String] Compatible libspotify API version.
