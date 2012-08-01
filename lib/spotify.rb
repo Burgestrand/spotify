aa8059a10b29f33f7bf86e616214de7153ad5025
Kim Burgestrand <kim@burgestrand.se>
2012-08-01 20:11:57 +0200

    Move .attach_function out to function-specific file

--- lib/spotify.rb
+++ lib/spotify.rb
@@ -28,37 +28,6 @@ class SpotifyAPI
     puts
     raise
   end
-
-  # @!macro [attach] attach_function
-  #   @!method $1(…)
-  #     @!scope class
-  #     @example method signature (shows arguments)
-  #       $*
-  #     @return [${-1}]
-  #
-  # Overloaded to ensure all methods are defined as blocking,
-  # and they return a managed pointer with the correct refcount.
-  #
-  # @param [#to_s] name function name sans `sp_` prefix.
-  # @param [Array] args
-  # @param [Object] returns
-  def self.attach_function(c_name = nil, name, args, returns)
-    if returns.respond_to?(:retaining_class) && name !~ /create/
-      returns = returns.retaining_class
-    end
-
-    options  = { :blocking => true }
-    c_name ||= "sp_#{name}"
-    super(name, c_name, args, returns, options)
-  end
-
-  # Now, make sure we have the right libspotify version.
-  attach_function :build_id, [], UTF8String
-  API_BUILD = SpotifyAPI.build_id
-
-  unless API_BUILD.include?(API_VERSION)
-    raise LoadError, "libspotify v#{build_id} is incompatible with ruby spotify v#{VERSION}(#{API_VERSION})"
-  end
 end
 
 require 'spotify/objects'
