88626a5e09784818cde9ab10ed02144053eb5957
Kim Burgestrand <kim@burgestrand.se>
2012-08-01 05:49:56 +0200

    Explicitly test Spotify::Struct

---
+++ spec/spotify/structs/struct_spec.rb
@@ -0,0 +1,34 @@
+describe Spotify::Struct do
+  let(:klass) do
+    Class.new(Spotify::Struct) do
+      layout :api_version => :int,
+             :cache_location => Spotify::NULString,
+             :user_agent => Spotify::NULString,
+             :compress_playlists => :bool
+    end
+  end
+
+  let(:options) do
+    {
+      :api_version => 1,
+      :cache_location => "Yay",
+      :user_agent => nil,
+      :compress_playlists => false
+    }
+  end
+
+  it "allows initializing the struct with a hash" do
+    struct = klass.new(options)
+    options.each_pair { |key, value| struct[key].should eq value }
+  end
+
+  it "allows initializing the struct with a pointer" do
+    original = klass.new(options)
+    struct = klass.new(original.pointer)
+    options.each_pair { |key, value| struct[key].should eq value }
+  end
+
+  it "allows initializing the struct with nothing" do
+    expect { klass.new }.to_not raise_error
+  end
+end
