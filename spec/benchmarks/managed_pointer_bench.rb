ce0dc31f15ebc3a7c4b3291bf4b07ca6bde5205d
Kim Burgestrand <kim@burgestrand.se>
2012-10-01 14:55:10 +0200

    Add benchmark for release as well

--- spec/benchmarks/managed_pointer_bench.rb
+++ spec/benchmarks/managed_pointer_bench.rb
@@ -2,6 +2,9 @@ module Spotify
   class << API
     def bogus_add_ref(x)
     end
+
+    def bogus_release(x)
+    end
   end
 
   class Bogus < ManagedPointer
@@ -16,6 +19,12 @@ bench "Album#to_native" do
   Spotify::Album.to_native(album, nil)
 end
 
-bench "Album#retain" do
+bench "Bogus#retain" do
   Spotify::Bogus.retain(one)
+  Spotify::Bogus.retaining_class.retain(one)
+end
+
+bench "Bogus#release" do
+  Spotify::Bogus.release(one)
+  Spotify::Bogus.retaining_class.release(one)
 end
