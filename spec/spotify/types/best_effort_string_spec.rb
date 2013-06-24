ef7ae9390b206be46a82016815d88ea1f8859095
Kim Burgestrand <kim@burgestrand.se>
2013-06-24 23:23:16 +0200

    Fix BestEffortString causing a warning when given UTF-8

--- spec/spotify/types/best_effort_string_spec.rb
+++ spec/spotify/types/best_effort_string_spec.rb
@@ -2,18 +2,21 @@
 describe Spotify::BestEffortString do
   describe "#to_native" do
     it "simply reads a null-terminated string" do
-      string = Spotify::BestEffortString.to_native("hello world", nil)
-      string.should eq "hello world"
+      source = "hello wörld"
+      string = Spotify::BestEffortString.to_native(source, nil)
+      string.should eq "hello wörld".force_encoding("BINARY")
     end
 
     it "cuts the string at the first encountered NULL-byte" do
-      string = Spotify::BestEffortString.to_native("hello\x00world", nil)
-      string.should eq "hello"
+      source = "hello\x00wörld".force_encoding("BINARY")
+      string = Spotify::BestEffortString.to_native(source, nil)
+      string.should eq "hello".force_encoding("BINARY")
     end
 
     it "treats empty string as empty string, and not null" do
-      string = Spotify::BestEffortString.to_native("", nil)
-      string.should eq ""
+      source = ""
+      string = Spotify::BestEffortString.to_native(source, nil)
+      string.should eq "".force_encoding("BINARY")
     end
   end
 end
