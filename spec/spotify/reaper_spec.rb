a2a303c07d320b454c2ec654f02ac7b6653e9658
Kim Burgestrand <kim@burgestrand.se>
2013-12-13 09:06:59 +0100

    Add a spec outline for the Reaper

---
+++ spec/spotify/reaper_spec.rb
@@ -0,0 +1,10 @@
+describe Spotify::Reaper do
+  # Our spec helper creates a new reaper for each spec, we are fine.
+  let(:reaper) { Spotify::Reaper.instance }
+
+  describe "#mark" do
+  end
+
+  describe "#terminate" do
+  end
+end
