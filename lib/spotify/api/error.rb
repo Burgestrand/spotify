d44eb27e25d57c7ae10f293003fdb9af5d978306
Kim Burgestrand <kim@elabs.se>
2014-02-21 15:33:13 +0100

    Add documentation to Error subsystem

--- lib/spotify/api/error.rb
+++ lib/spotify/api/error.rb
@@ -1,6 +1,9 @@
 module Spotify
   class API
     # @!group Error
+
+    # @param [Symbol, Integer] error
+    # @return [String] explanatory error message for an error code
     attach_function :error_message, [ :error ], UTF8String
   end
 end
